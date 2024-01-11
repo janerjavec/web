79d5d9ec24bebeb122dc12d465fdb41d5b1be61a
janerjavec <joneso.erjavec@gmail.com>
2024-01-11 17:51:25 +0100

    Future date.

--- Models/Reservation.cs
+++ Models/Reservation.cs
@@ -6,6 +6,27 @@ using web.Models;
 
 namespace web.Models
 {
+
+    public class FutureDateAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (!(value is DateTime))
+            {
+                return new ValidationResult("Neveljaven datum.");
+            }
+
+            DateTime submittedDate = (DateTime)value;
+            DateTime today = DateTime.Today;
+
+            if (submittedDate.Date < today)
+            {
+                return new ValidationResult("Datum mora biti današnji ali v prihodnosti.");
+            }
+
+            return ValidationResult.Success;
+        }
+    }
     public class Reservation
     {
         public int Id { get; set; }
@@ -13,10 +34,12 @@ namespace web.Models
         //public Guest Guest { get; set; }  // Navigation property to the Guest entity
         public int Room_Id { get; set; }
         [DataType(DataType.Date)]
+        [FutureDate]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         [Display(Name = "StartDate")]
         public DateTime StartDate { get; set; }
         [DataType(DataType.Date)]
+        [FutureDate]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         [Display(Name = "EndDate")]
         public DateTime EndDate { get; set; }
